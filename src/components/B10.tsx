37df0664811d5a4646819925c5e0e7ed472a9914
duong130125 <nguyendinhduong2005yt@gmail.com>
2024-06-05 15:46:54 +0700

    add

--- src/components/B10.tsx
+++ src/components/B10.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
 
 export default function B10() {
-    
+    function myFindIndex(arr: number[], item: number, callback: Function) {
+        let index = -1;
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i] === item) {
+            index = i;
+            break;
+          }
+        }
+        callback(index);
+      }
+
+      myFindIndex([1, 2, 3, 4, 5, 2], 2, (index) => {
+        console.log(index);
+      });
   return (
     <div>B10</div>
   )
